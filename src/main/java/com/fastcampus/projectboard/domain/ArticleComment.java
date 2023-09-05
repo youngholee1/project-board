0ad35f0c7b9f1758eda080fe3afacf5ddd9b353a
yougnho <yhl1173@naver.com>
2023-09-05 13:46:21 +0900

    1

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -3,7 +3,7 @@ package com.fastcampus.projectboard.domain;
 import java.time.LocalDateTime;
 
 public class ArticleComment {
-    private Long id;
+    private Long id;//
     private Article article;
     private String content;
     private LocalDateTime createdAt;
