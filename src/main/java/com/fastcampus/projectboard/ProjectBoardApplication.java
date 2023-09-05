5385981933f788f4a5983e35b614106607895374
yougnho <yhl1173@naver.com>
2023-09-05 11:00:52 +0900

    Å׽ºƮ

--- src/main/java/com/fastcampus/projectboard/ProjectBoardApplication.java
+++ src/main/java/com/fastcampus/projectboard/ProjectBoardApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectBoardApplication {
-
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectBoardApplication.class, args);
 	}
