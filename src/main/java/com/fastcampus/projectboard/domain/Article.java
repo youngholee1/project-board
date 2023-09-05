757de48382df3e6cca835ccabec36a6924e3a8a9
yougnho <yhl1173@naver.com>
2023-09-05 13:41:24 +0900

    Ŀ¹ÔÇմϴÙ

---
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String hashtag;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
